0bdc3ab5999a03f4b6cc3439a30792c3d6cd0d9a
PatrickChe <pchellakudam@gmail.com>
2023-10-04 10:25:22 +0200

    correction

--- video.php
+++ video.php
@@ -20,11 +20,5 @@ abstract class video implements ivideo{
     public function getSource(): String {
         return $this->source;
     }
-
-    public function getHTML(): String {
-        return "<iframe width='560' height='315' src='$this->source' 
-            title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; 
-            gyroscope; picture-in-picture; web-share' allowfullscreen></iframe>";
-    }
 }
 ?>
