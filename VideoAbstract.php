0fbb075b0d11235e06050e14777dd7be3a237be9
Arpit Singh <1054@htl.rennweg.at>
2024-10-14 22:17:31 +0200

    Interface implementiert und seine Klassen, Klasse VideoAbstract abstract gemacht

---
+++ VideoAbstract.php
@@ -0,0 +1,13 @@
+<?php
+abstract class VideoAbstract implements InterfaceVideo{
+    public function getTitel(){
+
+    }
+    public function getQuelle(){
+
+    }
+    public function getEmbettet(){
+        
+    }
+}
+?>
